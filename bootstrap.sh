294914baa30db097d7b3b4c1366a5d200d9f77c7
Jake Hiller <jakemhiller@gmail.com>
2019-01-23 12:27:26 -0500

    Remove unused brew dependencies

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,6 +11,8 @@ function doIt() {
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
+		--exclude "init" \
+		--exclude "brew.sh" \
 		-avh --no-perms . ~;
 	source ~/.bash_profile;
 }
